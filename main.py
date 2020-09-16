859a759c769d9c2db0d11140b0d04506611c2b7b
kaz101 <kaz101130@gmail.com>
2020-09-16 15:04:10 -0400

    stuff

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
 
 class World(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        #ShowBase.setBagroundColor(0,0,200)
-        self.track = loader.loadModel('/home/kaz/Downloads/BGP3D/Models/Track.egg')
+        self.track = loader.loadModel('BGP3D/Models/Track.egg')
         self.track.reparentTo(render)
+        self.camera.setZ(10)
+        self.accept('w-down',self.moveForward)
+        #taskMgr.add(self.moveForward, 'moveForward')
 
+    def moveForward(self):
+        self.camera.setY(self.camera.getY() + 1)
+        print('keyPressed')
+    def moveLeft(self):
+        self.camera.setX(self.camera.getX() - 1)
+        print('keyPressed')
+    def moveRight(self):
+        self.camera.setX(self.camera.getX() + 1)
+        print('keyPressed')
+    def moveBack(self):
+        self.camera.setY(self.camera.getY() - 1)
+        print('keyPressed')
 w = World()
+w.disableMouse()
+
+w.setBackgroundColor(0,0,200)
+
+w.accept('w',w.moveForward)
+w.accept('a',w.moveLeft)
+w.accept('s',w.moveBack)
+w.accept('d',w.moveRight)
 
 #base = ShowBase()
 w.run()
